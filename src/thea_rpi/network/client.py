462ce6e047ca49112ba20267f28d7e3f8e31745d
C Scott <scott2947.dev@gmail.com>
2026-02-17 21:56:50 +0000

    added udp support

--- src/thea_rpi/network/client.py
+++ src/thea_rpi/network/client.py
@@ -1,38 +1,79 @@
+from abc import ABC, abstractmethod
 import socket, struct
 from thea_rpi.config import SERVER_IP, PORT
 
 
-def start_connection() -> socket.socket:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((SERVER_IP, PORT))
-    print(f"CONNECTED TO {SERVER_IP}")
-    return s
+class BaseClient(ABC):
+
+    def __init__(self):
+        self.socket = None
 
 
-def close_connection(s : socket.socket) -> None:
-    print("CONNECTION CLOSED")
-    s.close()
+    @abstractmethod
+    def connect(self):
+        pass
 
 
-def send_data(s : socket.socket, data : bytes) -> None:
-    try:
-        length = len(data)
-        header = struct.pack(">I", length)
-        
-        s.sendall(header + data)
-        print("DATA SENT SUCCESSFULLY")
-
-    except Exception as e:
-        print(f"ERROR: {e}")
+    @abstractmethod
+    def send(self, data: bytes) -> None:
+        pass
 
 
-def send_text(s : socket.socket, text : str) -> None:
-    message = text.encode()
-    send_data(s, message)
+    def send_string(self, message: str) -> None:
+        data = message.encode("utf-16")
+        self.send(data)
+
+
+    def close(self) -> None:
+        if self.socket:
+            self.socket.close()
+            self.socket = None    
+
+
+class TCPClient(BaseClient):
+
+    def connect(self) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((SERVER_IP, PORT))
+        print(f"TCP Client connected to {SERVER_IP}:{PORT}")
+
+
+    def send(self, data: bytes) -> None:
+        if not self.socket:
+            raise RuntimeError("Client not connected. Call connect() first")
+
+        try:
+            header = struct.pack(">I", len(data))
+            self.socket.sendall(header + data)
+
+        except Exception as e:
+            print(f"TCP Client send error: {e}")
+
+
+    def close(self) -> None:
+        super().close()
+        print("TCP Client closed")
+
+
+class UDPClient(BaseClient):
+
+    def connect(self) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        print(f"UDP Client ready to send to {SERVER_IP}:{PORT}")
+
+
+    def send(self, data: bytes) -> None:
+        if not self.socket:
+            raise RuntimeError("Client not initialised")
+
+
+        self.socket.sendto(data, (SERVER_IP, PORT))
+
+
+    def close(self) -> None:
+        super().close()
+        print("UDP Client closed")
 
 
 if __name__ == "__main__":
-    s = start_connection()
-    message = "Hello, World!"
-    send_text(s, message)
-    close_connection(s)
+    pass
